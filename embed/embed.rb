4a4067a128e5d4344ab48bd692ae8abdb32c066a
Gregg Horton <greggahorton@gmail.com>
2015-05-17 15:08:48 -0700

    rust inside other languages

---
+++ embed/embed.rb
@@ -0,0 +1,13 @@
+require 'ffi'
+
+module Hello
+  extend FFI::Library
+  ffi_lib 'target/release/libembed.dylib'
+  attach_function :process, [], :void
+end
+
+binding.pry
+
+Hello.process
+
+puts "done!"
